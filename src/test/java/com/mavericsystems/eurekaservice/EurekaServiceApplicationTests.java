8e42d4ca07a2e394f5b614c86eec49502ed3a792
Vishnu Prasad K <vishnup@maveric-systems.com>
2022-03-21 15:29:35 +0530

    Eureka server code uploaded

---
+++ src/test/java/com/mavericsystems/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericsystems.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
